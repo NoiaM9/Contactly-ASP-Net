d8fc2e3ac5482eb580adcdf3925796dcd3a58b6f
NoiaM9 <cristian.business96@gmail.com>
2024-07-06 18:40:50 +0300

    Add project files.

---
+++ Contactly/Data/ContactlyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Contactly.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contactly.Data
+{
+    public class ContactlyDbContext : DbContext
+    {
+        //creez constructor pentru aceasta clasa
+        public ContactlyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
